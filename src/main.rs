d77fb106f0429fbf5e9eff403696e47a676eb376
Nejuf <nejuf1@gmail.com>
2016-06-11 17:39:14 -0700

    Adds guess reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+  println!("Guess the number!");
+
+  println!("Please input your guess.");
+
+  let mut guess = String::new();
+
+  io::stdin().read_line(&mut guess) //Returns a Result object that must have an error handler
+    .expect("Failed to read line"); //The expect() handler crashes/panics(exits) the program
+
+  println!("You guess: {}", guess);
 }
