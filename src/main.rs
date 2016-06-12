ada0c0c9ca3ed71f89eaeb4e97bbeb1a56793389
Nejuf <nejuf1@gmail.com>
2016-06-11 18:29:52 -0700

    Adds guess comparison

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -16,5 +17,15 @@ fn main() {
     .ok() //for compatibility with Rust v1.0.0 (where expect isn't a method on Result)
     .expect("Failed to read line"); //The expect() handler crashes/panics(exits) the program
 
-  println!("You guess: {}", guess);
+  let guess: u32 = guess.trim().parse()
+    .ok() //for compatibility with Rust v1.0.0
+    .expect("Please type a number!");
+
+  println!("You guessed: {}", guess);
+
+  match guess.cmp(&secret_number){
+    Ordering::Less => println!("Too small!"),
+    Ordering::Greater => println!("Too big!"),
+    Ordering::Equal => println!("You win!"),
+  }
 }
