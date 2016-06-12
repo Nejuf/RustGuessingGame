9dba66296be25409b139f84428f8a1a33de0302a
Nejuf <nejuf1@gmail.com>
2016-06-11 19:03:40 -0700

    Finishes the game by adding looping

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,32 @@ fn main() {
 
   let secret_number = rand::thread_rng().gen_range(1, 101);
 
-  println!("Please input your guess.");
+  loop{
+    println!("Please input your guess.");
 
-  let mut guess = String::new();
+    let mut guess = String::new();
 
-  io::stdin().read_line(&mut guess) //Returns a Result object that must have an error handler
-    .ok() //for compatibility with Rust v1.0.0 (where expect isn't a method on Result)
-    .expect("Failed to read line"); //The expect() handler crashes/panics(exits) the program
+    io::stdin().read_line(&mut guess) //Returns a Result object that must have an error handler
+      .ok() //for compatibility with Rust v1.0.0 (where expect isn't a method on Result)
+      .expect("Failed to read line"); //The expect() handler crashes/panics(exits) the program
 
-  let guess: u32 = guess.trim().parse()
-    .ok() //for compatibility with Rust v1.0.0
-    .expect("Please type a number!");
+    let guess: u32 = match guess.trim().parse() {
+      Ok(num) => num,
+      Err(_) => {
+        println!("Invalid number!");
+        continue;
+      },
+    };
 
-  println!("You guessed: {}", guess);
+    println!("You guessed: {}", guess);
 
-  match guess.cmp(&secret_number){
-    Ordering::Less => println!("Too small!"),
-    Ordering::Greater => println!("Too big!"),
-    Ordering::Equal => println!("You win!"),
-  }
-}
+    match guess.cmp(&secret_number){
+      Ordering::Less => println!("Too small!"),
+      Ordering::Greater => println!("Too big!"),
+      Ordering::Equal => {
+        println!("You win!");
+        break;
+      },
+    } // match guess.cmp(&secret_number){...}
+  } // loop {...}
+} // fn main(){...}
