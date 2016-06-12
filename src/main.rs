57abb2874222d687afa939c72cb57d821e930830
Nejuf <nejuf1@gmail.com>
2016-06-11 17:39:45 -0700

    Fixes incompatibility with Rust v1.0.0

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn main() {
   let mut guess = String::new();
 
   io::stdin().read_line(&mut guess) //Returns a Result object that must have an error handler
+    .ok() //for compatibility with Rust v1.0.0 (where expect isn't a method on Result)
     .expect("Failed to read line"); //The expect() handler crashes/panics(exits) the program
 
   println!("You guess: {}", guess);
